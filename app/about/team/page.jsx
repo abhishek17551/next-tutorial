38629e7b6d391e5e66dce988c5ad0bda33020519
abhishek17551 <ababhishek113@gmail.com>
2024-07-19 17:23:56 +0530

    implemented basic routing setup

---
+++ app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Team = () => {
+  return (
+    <div>
+        <h1>The Team:</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A iure rerum tempora suscipit, culpa deserunt tempore eum, non optio hic nesciunt, dignissimos sequi rem natus minima consequuntur ducimus perspiciatis exercitationem error? Impedit ipsam sed in quisquam, laboriosam vero ullam et illum eveniet, dolore veniam vitae deleniti labore error sit minus perferendis voluptatem itaque possimus at, corrupti nisi aperiam asperiores excepturi.</p>
+    </div>
+  )
+}
+
+export default Team
---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <ul>
+        <li><Link href='/'>Home</Link></li>
+        <li><Link href='/about'>About</Link></li>
+        <li><Link href='/about/team'>Team</Link></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Home
